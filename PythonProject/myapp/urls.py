dc5ea5d4e03ed3053951330feb1e7c813f06673a
Rindik <playmurka86@gmail.com>
2025-04-02 16:14:03 +0500

    Пути к страницам

--- PythonProject/myapp/urls.py
+++ PythonProject/myapp/urls.py
@@ -6,5 +6,6 @@ from . import views
 urlpatterns = [
 
 path('', views.home, name='home'),
-
+    path('register/', views.register, name='RegisterPage'),  # Уникальный URL
+    path('login/', views.login, name='LoginPage'),
 ]
