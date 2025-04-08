b4ef284d42fee30a92129cf856ca21a59b2e870d
RomanGatsapuk <gacapukroman4@gmail.com>
2025-04-08 23:38:47 +0500

    новые странички

--- PythonProject/myapp/urls.py
+++ PythonProject/myapp/urls.py
@@ -5,4 +5,8 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('register/', views.register, name='register'),
     path('login/', views.login, name='login'),
+    path('listing/', views.listing, name='listing'),
+    path('profile/', views.profile, name='profile'),
+    path('edit/', views.edit, name='edit'),
+    path('create/', views.create, name='create'),
 ]
--- PythonProject/myapp/views.py
+++ PythonProject/myapp/views.py
@@ -7,4 +7,17 @@ def register(request):
     return render(request, 'RegisterPage.html')
 
 def login(request):
-    return render(request, 'LoginPage.html')
+    return render(request, 'LoginPage.html')    return render(request, 'LoginPage.html')
+
+def listing(request):
+    return render(request, 'listing_list.html')
+
+def profile(request):
+    return render(request, 'profile.html')
+
+def edit(request):
+    return render(request, 'edit_list.html')
+
+def create(request):
+    return render(request, 'creeate_listing.html')
+
