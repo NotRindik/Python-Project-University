eb26fbe7cea22a3c09613ec16fa9bdef7c4f27b7
Dzmlv <40449@iitu.edu.kz>
2025-04-08 22:28:51 +0500

    функциональность кнопкам

--- PythonProject/myapp/urls.py
+++ PythonProject/myapp/urls.py
@@ -1,11 +1,8 @@
 from django.urls import path
-
 from . import views
 
-
 urlpatterns = [
-
-path('', views.home, name='home'),
-    path('register/', views.register, name='RegisterPage'),  # Уникальный URL
-    path('login/', views.login, name='LoginPage'),
+    path('', views.home, name='home'),
+    path('register/', views.register, name='register'),
+    path('login/', views.login, name='login'),
 ]
