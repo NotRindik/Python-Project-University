10954beecf5ec32572be40a8e8cc52798b76fff4
Dzmlv <40449@iitu.edu.kz>
2025-04-09 16:49:56 +0500

    база данных

--- PythonProject/myapp/forms.py
+++ PythonProject/myapp/forms.py
@@ -15,3 +15,11 @@ class CustomAuthenticationForm(AuthenticationForm):
 
     class Meta:
         fields = ['username', 'password']
+
+from django import forms
+from .models import Listing
+
+class ListingForm(forms.ModelForm):
+    class Meta:
+        model = Listing
+        fields = ['title', 'description', 'price', 'category', 'location', 'image']
