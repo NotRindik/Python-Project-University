99e0a40d5f8967f6af92d01919ec5b95c45ba0b9
RomanGatsapuk <gacapukroman4@gmail.com>
2025-04-16 11:05:44 +0500

    Выход

--- PythonProject/myapp/urls.py
+++ PythonProject/myapp/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path('', views.home, name='home'),
@@ -11,4 +12,5 @@ urlpatterns = [
     path('profile/', views.profile, name='profile'),
     path('edit/', views.edit, name='edit'),
     path('create/', views.create, name='create'),
+    path('logout/', LogoutView.as_view(next_page='home'), name='logout'),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
