fe837c02e6d29d114e1a71af1580431ae5acfc2f
Dzmlv <40449@iitu.edu.kz>
2025-04-16 11:31:38 +0500

    база данных

--- PythonProject/myapp/forms.py
+++ PythonProject/myapp/forms.py
@@ -1,23 +1,30 @@
-from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.models import User
 from django import forms
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import get_user_model
+from .models import Listing
+
+User = get_user_model()
 
 class CustomUserCreationForm(UserCreationForm):
     email = forms.EmailField(required=True)
+
     class Meta:
         model = User
         fields = ("username", "email", "password1", "password2")
 
 class CustomAuthenticationForm(AuthenticationForm):
-    username = forms.CharField(max_length=150, required=True, widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Username'}))
-    password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Password'}), required=True)
+    username = forms.CharField(
+        max_length=150,
+        required=True,
+        widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Username'})
+    )
+    password = forms.CharField(
+        widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Password'}),
+        required=True
+    )
 
     class Meta:
         fields = ['username', 'password']
-
-from django import forms
-from .models import Listing
 
 class ListingForm(forms.ModelForm):
     class Meta:
